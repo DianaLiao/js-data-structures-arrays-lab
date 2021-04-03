0f4d079ea3a60ee285311fcca7846f259a1b7d37
Diana Liao <dijali@gmail.com>
2021-04-03 18:17:39 -0400

    commit tree 92443189c12b6b93d81b24c5c3457e1d5a60e593

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  // let driversCopy = drivers.slice(0);
+  // driversCopy.push(name);
+  // return driversCopy;  
+
+  return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+function removeLastDriver() {
+  let limit = drivers.length -1;
+  return drivers.slice(0,limit);
+}
+
+function removeFirstDriver(){
+  return drivers.slice(1);
+}
